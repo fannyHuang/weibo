671b240d0beed17cc235eda25008e4c8a45471b8
fang <491585767@qq.com>
2019-07-04 03:48:54 +0000

    命名路由

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+	<img class="brand-icon" src="img/8.jpg">
+	<a href="{{ route('home') }}" target="_blank">
+		读万卷书 行万里路
+	</a>
+	<div class="float-right">
+		<a href="{{route('about')}}">关于</a>
+		
+	</div>
+	
+</footer>
