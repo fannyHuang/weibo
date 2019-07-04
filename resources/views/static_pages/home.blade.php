b16216f2f9ffdc214e324d018f9b3e189eaacd1c
fang <491585767@qq.com>
2019-07-04 02:47:56 +0000

    静态文件浏览器缓存问题

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.default')
 
 @section('content')
-	<h1>主页</h1>
+	<div class="jumbotron">
+		<h1>hello guys</h1>
+		<p class="lead">
+			您现在所看到的的是 xiaofang的微博主页~
+		</p>
+		<p>一切，将从这里开始。</p>
+		<p>
+			<a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+		</p>
+	</div>
+	
 @stop
