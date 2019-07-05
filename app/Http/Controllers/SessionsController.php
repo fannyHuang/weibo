d541ebb27e55532858882d468a3447274b24d787
fang <491585767@qq.com>
2019-07-05 02:10:58 +0000

    登录成功后导航逻辑

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -16,10 +16,10 @@ class SessionsController extends Controller
    		]);
    		if(Auth::attempt($v_res)){
    			//登陆成功的操作
-   			session()->flash('success',"欢迎回来");
+   			session()->flash('success',"欢迎回来!");
    			return redirect()->route('users.show',[Auth::user()]);
    		}else{
-   			session()->flash('danger','抱歉，邮箱或密码不匹配')；
+   			session()->flash('danger','抱歉，邮箱或密码不匹配');
    			return redirect()->back()->withInput();
    			//登陆失败的操作	
    		}
--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -18,7 +18,7 @@
 					<input type="text" name="email" class="form-control" value="{{ old('email')}}">
 				</div>
 				<div class="form-group">
-					<label for="password">邮箱:</label>
+					<label for="password">密码:</label>
 					<input type="password" name="password" class="form-control" value="{{ old('password')}}">
 				</div>
 
