07ec2046f5f8be3378c4d315f970b70919a6cbf1
fang <491585767@qq.com>
2019-07-05 03:01:27 +0000

    更新个人资料

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -11,9 +11,9 @@
   					</a>
   					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
   						<a class="dropdown-item" href="{{ route('users.show',Auth::user())}}">个人中心</a>
-  						<a class="dropdown-item" href="#">编辑资料</a>
+  						<a class="dropdown-item" href="{{route('users.edit',Auth::user())}}">编辑资料</a>
   						<div class="dropdown-divider" ></div>
-  						<a class="dropdown-item" href="3" id="logout">
+  						<a class="dropdown-item" href="#" id="logout">
   							<form action="{{route('logout')}}" method="POST">
   								{{csrf_field()}}
   								{{method_field('DELETE')}}
